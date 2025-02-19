4e189e835f2613998193d7b392f4cebfd488a92b
Ron Phillips <mcspidy@gmail.com>
2025-02-19 11:45:55 -0600

    Solution

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-k$e3#$$&3vjylpvbg+8&+mcj1*(juauij*q+nq!8uw=&x!5qm-"
+# for security reasons, the secret key is stored in a .env file
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# for security reasons, the DEBUG value is stored in a .env file
+DEBUG = os.getenv("DEBUG", False)
 
 ALLOWED_HOSTS = []
 
